9837271aadfcb84db28935545cbf241f2a94939e
Macbook <macbook@MacBook-Air-Macbook.local>
2024-11-23 23:24:46 +0400

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,24 @@
+import Logo from '../img/logo.png'
+import ArrowDown from '../img/Arrow - Down.svg'
+
+function Header() {
+    return(
+        <div className='firstBg'> 
+            <header className="container flex items-center h-18 justify-between font-bold">
+                <img src={Logo} alt="" />
+                <nav className='flex gap-16'>
+                    <a href="#">Episodes</a>
+                    <a href="#">About</a>
+                    <a href="#" className='flex gap-1'>More <img src={ArrowDown} alt="" /></a>
+                </nav>
+                <div className='flex gap-5'>
+                    <button className='px-6 py-3 border-2 rounded-lg border-black'>RECENT EPISODES</button>
+                    <button className='subscribe px-6 py-3 border-2 rounded-lg border-black bg-black text-white'>SUBSCRIBE</button>
+                </div>
+            </header>
+        </div>
+        
+    )
+}
+
+export default Header;
